584afa4d44b45af95491464c8b4e1f50ef4339d9
kinaoshi <ku.928.maid@gmail.com>
2021-09-14 21:10:18 +0900

    ✨ 新規作成ファイル

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,23 @@
+
+import Link from "next/link";
+
+export const Header = () =>  {
+	
+
+	return (
+		<div className="w-auto mt-8 mx-10">
+			<div className="flex ">
+				<Link href="/">
+					<a className="mx-2">index</a>
+				</Link>
+				<Link href="/second">
+					<a className="mx-2">second</a>
+				</Link>
+				<Link href="/third">
+					<a className="mx-2">third</a>
+				</Link>
+			</div>
+		
+		</div>
+	);
+}
