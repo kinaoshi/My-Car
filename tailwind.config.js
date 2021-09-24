1d245f83bfb498eb3750ccf0fb893b21395a3575
kinaoshi <ku.928.maid@gmail.com>
2021-09-24 21:23:18 +0900

    💫 HOMEの画像

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,27 @@
 module.exports = {
-  "editor.quickSuggestions": { "strings": true },
-  mode:"jit",
-  purge: ['src/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+	"editor.quickSuggestions": { strings: true },
+	mode: "jit",
+	purge: ["src/**/*.{js,ts,jsx,tsx}"],
+	darkMode: false, // or 'media' or 'class'
 
-  theme: {
-    extend: {
-      fontFamily: {
-        fantasy: "fantasy",
-        Courier:"Courier",
-
-      },
-      fontFamily: {
-        Arial:"Arial",
-      },
-
-    },
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-
-}
+	theme: {
+		extend: {
+			fontFamily: {
+				fantasy: "fantasy",
+				Courier: "Courier",
+			},
+			fontFamily: {
+				Arial: "Arial",
+			},
+		},
+    // screens: {
+    //   hlg: {
+    //     raw: '(min-width: 780px) and (min-height: 300px)'
+    //   }
+    // },
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [],
+};
