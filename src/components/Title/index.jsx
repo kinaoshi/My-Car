f6f65416b7db85f0882ef16f252ae02e9925a997
kinaoshi <ku.928.maid@gmail.com>
2021-10-21 09:24:30 +0900

    イタリック

--- src/components/Title/index.jsx
+++ src/components/Title/index.jsx
@@ -4,8 +4,8 @@ export const Title = (props) => {
 	return (
 	
 					<div className="flex flex-wrap mt-4">
-						<p className="xl:hover:bg-purple-300 font-bold font-Arial  text-red-600 text-6xl  text-left ml-10">
-							BMW525-D
+						<p className="italic xl:hover:bg-purple-300 font-bold font-Arial  text-red-600 text-6xl  text-left ml-10 text-">
+							BMW525 d
 						</p>
 						<p className=" font-bold p-2 text-yellow-900 text-4xl   ml-6">
 							{props.title}
